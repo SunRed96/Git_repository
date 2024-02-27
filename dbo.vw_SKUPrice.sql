f2e2e76f99112767cce22aa9518528b92fa86ddd
Korbut.Egor <pol-kg-i-96-g@mail.ru>
2024-02-27 21:37:33 +0300

    The answer to task 4

---
+++ dbo.vw_SKUPrice.sql
@@ -0,0 +1,10 @@
+﻿IF EXISTS(select * FROM sys.views where name = 'vw_SKUPrice') 
+Drop View vw_SKUPrice;
+
+go
+
+Create VIEW vw_SKUPrice as
+	Select *, [dbo].[udf_GetSKUPrice]([ID]) as Unit_Price 
+	From [dbo].[SKU]
+
+
